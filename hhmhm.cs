90c8fec670e183896d974cd9c7a5a37986d77c11
PRATHAP44 <erraguntlaprathap@gmail.com>
2022-02-07 17:31:40 +0530

    this program editing

---
+++ hhmhm.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0,b = 1,c,n,i;
+            Console.WriteLine("Enter number for fibonacci:"); 
+            n=int.Parse(Console.ReadLine());
+            Console.Write(a+" "+b+" ");
+            for(i=0;i<=n;i++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a=b;
+                b=c;
+            }
+
+        }
+    }
+}
